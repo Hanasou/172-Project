e029ca5bfdaf3578e3d8632c8015d59543eb3fb8
LetTheBassKick <royzhang42@gmail.com>
2019-10-20 00:24:54 -0700

    changed ip

--- basic.py
+++ basic.py
@@ -122,6 +122,6 @@ def index():
     return render_template('index.html', card_list = card_list)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host="0.0.0.0", port=80)
     
 
